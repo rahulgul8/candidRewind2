521ccf89f2ef24f7d5a128d380513a7a94e92084
rahul <rahulgul8@gmail.com>
2019-08-26 17:14:45 +0530

    Adding initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { MenuComponent } from './menu/menu.component';
+import { CenterMenuComponent } from './center-menu/center-menu.component';
+import { MaterialModule } from './material-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,MenuComponent, CenterMenuComponent, HomeComponent, ContactUsComponent, GalleryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
